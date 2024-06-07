31c06d2359b3ed412081f894023ef2406f571358
Matthias Mair <code@mjmair.com>
2024-06-07 22:07:57 +0200

    Add error message if host/token not set

--- inventree_tui/api/base.py
+++ inventree_tui/api/base.py
@@ -1,7 +1,23 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 from inventree.api import InvenTreeAPI
+
 load_dotenv()
 host = os.environ.get("INVENTREE_API_HOST")
 token = os.environ.get("INVENTREE_API_TOKEN")
+
+def error_msg(name, envname, sample):
+    print(f"{name} not set - set `{envname}` to fix this\nExample: `export {envname}={sample}`\n")
+
+if not host or not token:
+    print("Missing configuration\n")
+
+    if not host:
+        error_msg("InvenTree API host", "INVENTREE_API_HOST", "https://inventree.localhost")
+
+    if not token:
+        error_msg("InvenTree API token", "INVENTREE_API_TOKEN", "inv-f0e03dc3a7e0a6421ffba5d219858f52a85da3ca-20240607")
+    exit(1)
+
 api = InvenTreeAPI(host=host, token=token)
